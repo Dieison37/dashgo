1e410d94aba32e4e7738f18ad72a38cb3271ee50
Dieison Lemes Vieira <dieisonambiental@gmail.com>
2022-08-24 13:00:29 -0300

    Add files via upload

---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,51 @@
+import { ReactNode } from "react";
+import { useQuery } from "react-query";
+import { api } from "../api";
+
+type User = {
+ createdAt: ReactNode;
+ id: string;
+ name: string;
+ email: string;
+ createAt: string;
+}
+
+type GetUsersResponse = {
+  [x: string]: any;
+  totalCount: number;
+  users: User[];
+}
+
+async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('users', {
+    params: {
+      page, 
+    }
+  })
+
+  const totalCount = Number(headers['x-total-count'])
+
+  const users = data.users.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', { 
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
+
+  return { 
+    users,
+    totalCount,
+  };
+}
+export function useUsers(page: number) {
+  return useQuery(['users', page], () => getUsers(page), {
+    staleTime: 1000 * 60 * 10, // 10 minutes
+    
+ })
+}
